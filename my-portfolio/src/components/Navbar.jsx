14c2b2340a4de6db52084b7bcd390de438189bf2
Hackerdude374 <robertle982@gmail.com>
2024-02-28 12:59:31 -0500

    navbar

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar = () => {
   return (
    <nav 
    className = {`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-    <div 
-    className = "w-full flex justify-between items-center max-w-7xl mx-auto">
+    <div className = "w-full flex justify-between
+     items-center max-w-7xl mx-auto">
       <Link
       to = "/"
       className= "flex items-center gap-2"
@@ -20,9 +20,11 @@ const Navbar = () => {
         window.scrollTo(0,0);
       }}
       >
-      <img src = {logo} alt = "logo" className = "w-9 h-9 object-contain" />
-      <p classname = "text-white text -[18px] font-bold cursor-pointer">Robert <span className = "sm:block hidden">| Portfolio /robertle982@gmail.com</span></p>
+      <img src = {logo} alt = "logo" className = "w-9
+       h-9 object-contain" />
+      <p classname = "text-white text-[18px] font-bold cursor-pointer">Robert <span className = "sm:block hidden">| Portfolio /robertle982@gmail.com</span></p>
       </Link>
+      <p className = "text-red-500"> asdsa</p>
     </div>
    </nav>
   )
