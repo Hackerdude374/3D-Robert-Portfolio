0762b318cd677eb01dc80005d46b1bcc3a4fa37d
Hackerdude374 <robertle982@gmail.com>
2024-03-03 00:10:05 -0500

    changed contact form email

--- my-portfolio/src/components/Contact.jsx
+++ my-portfolio/src/components/Contact.jsx
@@ -44,9 +44,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Robert Le",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "robertle982@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
